d4d3bb7840926ef7951e1bf29a19572fb90b7638
İbrahim Koçal <enesibo53@hotmail.com>
2023-12-15 20:30:21 +0300

    load more games özelliği ve butonlara yönlendirme
    daha fazla göster dedikten sonra yükleme logosu ve ardından daha fazla oyun kartı görünmesi sağlandı. sayfadaki bazı linklere hedef verildi

--- js/index.js
+++ js/index.js
@@ -47,22 +47,19 @@ var dropdownDiv = document.getElementById("dropdownDiv");
 var floatingButton = document.getElementById("floatingButton");
 
 toggleButton.addEventListener("click", function(event) {
-    event.stopPropagation(); // DropdownDiv açıldığında document click olayını engelle
+    event.stopPropagation();  
     dropdownDiv.classList.toggle("hidden");
-    closeButton.style.display = "block"; // Menü butonuna tıklanınca çarpı butonunu göster
+    closeButton.style.display = "block"; 
     floatingButton.style.display = "none";
 });
 
 closeButton.addEventListener("click", function(event) {
-    event.stopPropagation(); // Menü butonuna tıklandığında çarpı butonuna basılmamasını sağla
+    event.stopPropagation();  
     dropdownDiv.classList.add("hidden");
-    closeButton.style.display = "none"; // Dropdown kapatıldığında çarpı butonunu gizle
+    closeButton.style.display = "none"; 
     floatingButton.style.display = "block";
 });
 
-
-
-// JavaScript kullanarak sayfa yüklenirken yükleme logosunu ve daha fazla oyunu göster
 function loadMoreGames() {
     var loadMoreButton = document.getElementById("load-more-button");
     var loadingLogo = document.getElementById("loading-logo");
@@ -71,7 +68,7 @@ function loadMoreGames() {
     // Simüle edilmiş bir asenkron veri yükleme işlemi (örnek olarak setTimeout kullanılıyor)
     setTimeout(function () {
         // Yükleme logosunu göster
-        loadingLogo.style.display = "block";
+        loadingLogo.classList.remove("d-none");
         // "Daha Fazla Göster" butonunu gizle
         loadMoreButton.style.display = "none";
 
@@ -81,43 +78,43 @@ function loadMoreGames() {
             var newGamesHTML = "";
             for (var i = 0; i < 3; i++) {
                 newGamesHTML += `
-                    <div class="col-12 col-xl-3 col-lg-4 col-sm-6">
-                        <div class="card bg-dark mb-5">
-                            <img class="card-img-top img-fluid" src="img/gamesList/tlou.jpg" alt="Sample Game">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-between">
-                                    <div>
-                                        <p class="card-text gameName">Sample Game ${i + 1}</p>
-                                        <i class="fab fa-2x fa-steam me-2" style="color: #ffffff;"></i>
-                                        <i class="fab fa-2x fa-windows" style="color: #ffffff;"></i>
-                                    </div>
-                                    <div>
-                                        <div class="align-self-start text-center mt-2">
-                                            <h6>Price ${i + 1}</h6>
-                                            <p>Discounted Price ${i + 1}</p>
-                                        </div>  
-                                        <div class="align-self-end text-center mt-4">
-                                            <a href="#">
-                                                <i class="btn-sm shop fas fa-2x fa-shopping-basket align-self-end"></i>
-                                            </a>  
-                                        </div> 
-                                    </div>      
-                                </div>    
-                            </div> 
-                        </div>    
+                <div class="col-12 col-xl-3 col-lg-4 col-sm-6">
+                <div class="card bg-dark mb-5">
+                    <a href="gamesDetails/tlou.html"><img class="card-img-top img-fluid" src="img/gamesList/tlou.jpg" alt="The Last of Us"></a>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <p class="card-text gameName">The Last of Us</p>
+                                <i class="fab fa-2x fa-steam me-2" style="color: #ffffff;"></i>
+                                <i class="fab fa-2x fa-windows" style="color: #ffffff;"></i>
+                            </div>
+                            <div>
+                                <div class="align-self-start text-center mt-2">
+                                    <h6>1734,09 ₺</h6>
+                                    <p>1168.73 ₺</p>
+
+                                </div>
+                                <div class="align-self-end text-center mt-4">
+                                    <a href="#">
+                                        <i class="btn-sm shop fas fa-2x fa-shopping-basket align-self-end"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </div>
                 `;
             }
 
             additionalGames.innerHTML += newGamesHTML;
 
             // Yükleme logosunu gizle
-            loadingLogo.style.display = "none";
-            // "Daha Fazla Göster" butonunu göster
-            loadMoreButton.style.display = "block";
+            loadingLogo.classList.add("d-none");
+                        // "Daha Fazla Göster" butonunu göster
+            loadMoreButton.style.display("block");
 
         }, 2000); // Örnek olarak 2 saniye sonra yükleme tamamlanıyor gibi düşünülebilir
 
     }, 2000); // Örnek olarak 2 saniye sonra yükleme tamamlanıyor gibi düşünülebilir
 }
-
